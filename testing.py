c4b4c8fb1424be4b1b60a8433507cb94a0790737
Sean Bonjean <sean-bonjean@outlook.com>
2025-10-01 19:17:54 +0800

    fix: adding more template scripts

--- testing.py
+++ testing.py
@@ -28,4 +28,12 @@ data = node_worker.pass_through_shard(data)
 
 # transfer_data(data, save_path="results/data.pt")
 
-reached_eos, data = node_worker.receive_next_token(data)
+first_node = NodeWorker(
+    can_receive_user_request=True,
+    # shards_path="shards/Llama-2-7b-chat-hf_float16",
+    shards_path="C:/Users/sean-/Desktop/shards/Llama-2-7b-chat-hf_float16",  # 权重shard暂存SSD，避免硬盘瓶颈
+    device="cuda:0",
+    dtype=torch.float16
+)
+first_node.load_shards(0, 1)
+reached_eos, data = first_node.receive_next_token(data)
